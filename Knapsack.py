1ce0b6117beb6fe8ace40782dcc4b3e2ecbee67f
ananyyanair <ananya.ai22@bmsce.ac.in>
2024-07-16 20:09:25 +0530

    Update Knapsack.py

--- Knapsack.py
+++ Knapsack.py
@@ -16,27 +16,17 @@ import sys
 #
 
 def unboundedKnapsack(k, arr):
-    memo = [0] * (k + 1)
+    memo = [0] * (k + 1)    
     for i in range(len(arr)):
         for j in range(arr[i], k + 1):
-            memo[j] = max(memo[j], arr[i] + memo[j - arr[i]])
+            memo[j] = max(memo[j], arr[i] + memo[j - arr[i]])    
     return memo[k]
 
 if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
     t = int(input().strip())
-
-    first_multiple_input = input().rstrip().split()
-
-    n = int(first_multiple_input[0])
-
-    k = int(first_multiple_input[1])
-
-    arr = list(map(int, input().rstrip().split()))
-
-    result = unboundedKnapsack(k, arr)
-
-    fptr.write(str(result) + '\n')
-
-    fptr.close()
+    for _ in range(t):
+        n, k = map(int, input().strip().split())
+        arr = list(map(int, input().strip().split()))
+        
+        result = unboundedKnapsack(k, arr)
+        print(result)
